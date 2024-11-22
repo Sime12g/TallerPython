6593278794c7c2d4bb4524c707ca6a0c76f53940
Xayli Ximena <l22240071@smartin.tecnm.mx>
2024-11-22 15:50:29 -0600

    Iterador1

---
+++ Iteradores/Iterador1.py
@@ -0,0 +1,28 @@
+def generador1():
+    for i in range(0,20):
+        yield i
+
+def generador2():
+    for i in range(0,20):
+        if i%2 == 0:
+            yield f"{i} es par"
+        else:
+            yield f"{i} es impar"
+
+if __name__ == '__main__':
+    rango1=generador1()
+    rango2 = generador2()
+
+    print(type(rango1))
+
+    #print(next(rango1))
+    #print(next(rango1))
+    #print(next(rango1))
+    #print(next(rango1))
+#dependiendo los que pongas son los numeros que te va a dar
+    print(next(rango2))
+    print(next(rango2))
+    print(next(rango2))
+    print(next(rango2))
+    print(next(rango2))
+    print(next(rango2))
